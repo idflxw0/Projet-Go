e11bcdfbe4e94d146e2ca4d786b405fe63d3fc0c
idfl0wx <idfl0wxzero@gmail.com>
2023-11-19 18:59:18 +0100

    args par cmd

--- src/main.java
+++ src/main.java
@@ -5,38 +5,33 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Bienvenue dans le jeu de Go");
-
+        if(args.length != 2 || !args[0].equals("boardsize")) {
+            System.out.println("Usage: GameGo <board size> <size>");
+            System.exit(1);
+        }
         int size = 0;
-        IBoard board = null;
+        try {
+            size = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            System.out.println("Taille du plateau invalide: " + args[1]);
+            System.exit(1);
+        }
+        IBoard board = new Board(size);
+        System.out.println("Plateau de taille " + size + " créé");
 
-        System.out.println("Veuillez choisir la taille du plateau : ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entrez une commande : ");
         String input = sc.nextLine();
 
         while (!input.equals("exit")) {
-            try {
-                if (input.contains("boardsize")) {
-                    size = Integer.parseInt(input.split(" ")[1]);
-                    board = new Board(size);
-                    System.out.println("Plateau de taille " + size + " créé");
-                } else if (input.contains("showboard")) {
-                    if (board != null) {
-                        board.showBoard();
-                    } else {
-                        System.out.println("Le plateau n'a pas été initialisé avec une taille.");
-                    }
-                } else {
-                    System.out.println("Commande inconnue");
-                }
-            } catch (NumberFormatException e) {
-                System.out.println("Veuillez entrer un nombre valide pour la taille du plateau.");
+            if (input.contains("showboard")) {
+                board.showBoard();
+            } else {
+                System.out.println("Commande inconnue");
             }
-
             System.out.println("Entrez une commande : ");
             input = sc.nextLine();
         }
-
         sc.close();
     }
 }
